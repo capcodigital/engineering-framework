5660e8eefc8eaaea3ecff334288bbd0082858708
vicky <vicky.dineshchandra@capoc.com>
2021-06-25 16:29:38 +0100

    update author

--- eureka_identity.sh
+++ eureka_identity.sh
@@ -6,5 +6,5 @@ TITLE=Engineering Frameworks
 IMG_URL=https://d15shllkswkct0.cloudfront.net/wp-content/blogs.dir/1/files/2018/07/28058888_508880689513511_3062259406262730544_n.png
 DESCRIPTION=Engineering framework
 VERSION=1.0
-OWNER=Alexandra Veres
+OWNER=Sandra
 OWNER_IMG_URL=https://media-exp1.licdn.com/dms/image/C4D03AQHLh6FoCY9nCA/profile-displayphoto-shrink_400_400/0/1614023904908?e=1625702400&v=beta&t=ThvBEOosrUJz462mLn8gTDbr59ntD78ZQanu25_KcGU
